993e3cf86b47e821c1752ecb58375eca18a3b00d
Jérémy DECOOL <contact@jdecool.fr>
2020-09-05 22:20:56 +0200

    Fix PublishFactory tests

--- tests/PublisherFactoryTest.php
+++ tests/PublisherFactoryTest.php
@@ -14,7 +14,7 @@ class PublisherFactoryTest extends TestCase
     private const URL = 'https://demo.mercure.rocks/.well-known/mercure';
     private const JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIqIl0sInB1Ymxpc2giOlsiKiJdfX0.M1yJUov4a6oLrigTqBZQO_ohWUsg3Uz1bnLD4MIyWLo';
 
-    public function createDefaultPublisher(): void
+    public function testCreateDefaultPublisher(): void
     {
         $jwtProvider = function(): string {
             return self::JWT;
